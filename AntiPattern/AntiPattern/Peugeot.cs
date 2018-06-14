8e166e6fd1d8a206300272d6ba3e04e613ecd03c
Евгений-ПК\Евгений <jeniamasarn94@gmail.com>
2018-06-14 11:19:20 +0300

    Revert "+add comments"
    This reverts commit 3b3fd474f53890f5527319209d08ecccec9eb6f3.

--- AntiPattern/AntiPattern/Peugeot.cs
+++ AntiPattern/AntiPattern/Peugeot.cs
@@ -9,7 +9,6 @@ namespace AntiPattern
 {
     class Peugeot: IVehicle
     {
-        //add comments
         public string model;
         public int dateOfManufacture;
         public int amountOfWheels = 4;
